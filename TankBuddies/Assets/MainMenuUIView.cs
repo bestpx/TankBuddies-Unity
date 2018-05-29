4e2dce6b55ce0558dca51454c6b7cd131313988e
Dennis Pan <bestpx@gmail.com>
2018-05-28 17:42:33 -0700

    Main menu

---
+++ TankBuddies/Assets/MainMenuUIView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TankBuddies.UI
+{
+
+    public class MainMenuUIView : MonoBehaviour
+    {
+        public void OnStartButtonClicked()
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+
+        public void OnQuitButtonClicked()
+        {
+            Application.Quit();
+        }
+    }
+
+}
